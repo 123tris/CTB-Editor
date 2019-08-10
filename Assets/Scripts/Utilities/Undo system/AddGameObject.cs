053aec1b2ca86927f9b36c80880eacb3e1cfa62e
123tris <tristan.a.metz@gmail.com>
2019-08-10 19:09:54 +0200

    Moved memento's outside of the undo class

---
+++ Assets/Scripts/Utilities/Undo system/AddGameObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace RuntimeUndo
+{
+    public class AddGameObject : IMemento<RemoveGameObject>
+    {
+        private GameObject addedObject;
+
+        public AddGameObject(GameObject obj)
+        {
+            addedObject = obj;
+        }
+
+        public RemoveGameObject Revert()
+        {
+            GameObject copy = Object.Instantiate(addedObject,GameManager.garbage.transform);
+            RemoveGameObject removedObject = new RemoveGameObject(copy,addedObject.transform.parent);
+            Object.Destroy(addedObject);
+            return removedObject;
+        }
+    }
+}
