851bfe14345324b066a544c51dba084d0283e8c9
123tris <tristan.a.metz@gmail.com>
2019-08-25 15:17:04 +0200

    New undo templates

---
+++ Assets/Scripts/Utilities/Undo system/AddGameObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RuntimeUndo
+{
+    public class AddGameObjects : IMemento<RemoveGameObjects>
+    {
+        private List<GameObject> addedObjects;
+
+        public AddGameObjects(List<GameObject> objects)
+        {
+            addedObjects = objects;
+        }
+
+        public RemoveGameObjects Revert()
+        {
+            //TODO: temporarily uses first index for testing purposes. However it should be changed to a list of parents so that every gameobject can have a unique parent if that will ever become necessary
+            RemoveGameObjects removedObject = new RemoveGameObjects(addedObjects,addedObjects[0].transform.parent); 
+            foreach (GameObject addedObject in addedObjects)
+            {
+                Object.Destroy(addedObject);
+            }
+            addedObjects.Clear();
+            return removedObject;
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/Undo system/RemoveGameObjects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RuntimeUndo
+{
+    public class RemoveGameObjects : IMemento<AddGameObjects>
+    {
+        private List<GameObject> removedObjects = new List<GameObject>();
+        private Transform parent;
+
+        public RemoveGameObjects(List<GameObject> objects, Transform parent)
+        {
+            foreach (GameObject gameObject in objects)
+            {
+                removedObjects.Add(Object.Instantiate(gameObject,GameManager.garbage.transform));
+            }
+            this.parent = parent;
+        }
+
+        public AddGameObjects Revert()
+        {
+            List<GameObject> addedObjects = new List<GameObject>();
+            foreach (GameObject removedObject in removedObjects)
+            {
+                GameObject addedObject = Object.Instantiate(removedObject, parent);
+                HitObject hitobject = addedObject.GetComponent<HitObject>();
+                if (hitobject != null)
+                    hitobject.Init(hitobject.transform.position - Grid.Instance.transform.position); //TODO: Clean up dirty code
+                addedObjects.Add(addedObject);
+            }
+            return new AddGameObjects(addedObjects);
+        }
+    }
+}
