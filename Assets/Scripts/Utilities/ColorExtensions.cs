80b38280d3676b16471cb37fba64c927eb40c9d7
123tris <tristan.a.metz@gmail.com>
2019-08-25 15:18:44 +0200

    Added compilation utility class

---
+++ Assets/Scripts/Utilities/ColorExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class ColorExtensions
+{
+    public static Color OverrideAlpha (this Color color, float a)
+    {
+        return new Color(color.r,color.g,color.b,a); //
+    }
+}
